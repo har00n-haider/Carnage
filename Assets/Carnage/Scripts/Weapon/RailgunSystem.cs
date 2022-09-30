8cda50855f5562ef6ff3268660a774c73b6075fc
har00n-haider <72197495+har00n-haider@users.noreply.github.com>
2022-09-30 11:00:45 +0100

    Remove netcode references from the rail gun system

--- Assets/Carnage/Scripts/Weapon/RailgunSystem.cs
+++ Assets/Carnage/Scripts/Weapon/RailgunSystem.cs
@@ -4,7 +4,6 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
-using Unity.NetCode;
 using Unity.Physics;
 using Unity.Physics.Systems;
 using Unity.Transforms;
@@ -14,7 +13,6 @@ using Unity.Transforms;
 public partial class RailgunSystem : SystemBase
 {
     public BuildPhysicsWorld BuildPhysicsWorld;
-    public GhostPredictionSystemGroup GhostPredictionSystemGroup;
     public WeaponCommandBufferSystem WeaponCommandBufferSystem;
 
     private Unity.Mathematics.Random _random;
@@ -24,7 +22,6 @@ public partial class RailgunSystem : SystemBase
         base.OnCreate();
 
         BuildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
-        GhostPredictionSystemGroup = World.GetOrCreateSystem<GhostPredictionSystemGroup>();
         WeaponCommandBufferSystem = World.GetOrCreateSystem<WeaponCommandBufferSystem>();
 
         _random = Unity.Mathematics.Random.CreateFromIndex((uint)0);
@@ -41,8 +38,6 @@ public partial class RailgunSystem : SystemBase
     {
         float deltaTime = Time.DeltaTime;
         float elapsedTime = (float)Time.ElapsedTime;
-        uint tick = GhostPredictionSystemGroup.PredictingTick;
-        bool isServer = World.GetExistingSystem<ServerSimulationSystemGroup>() != null;
 
         EntityCommandBuffer commandBuffer = WeaponCommandBufferSystem.CreateCommandBuffer();
         CollisionWorld collisionWorld = BuildPhysicsWorld.PhysicsWorld.CollisionWorld;
@@ -56,119 +51,111 @@ public partial class RailgunSystem : SystemBase
             .ForEach((
                 Entity entity,
                 ref Weapon weapon,
-                ref Railgun railgun,
-                in PredictedGhostComponent predictedGhost) =>
+                ref Railgun railgun) =>
             {
-                if (GhostPredictionSystemGroup.ShouldPredict(tick, predictedGhost))
+
+                railgun._firingTimer -= deltaTime;
+
+                if (weapon.ShootRequested && railgun._firingTimer <= 0f)
                 {
-                    railgun._firingTimer -= deltaTime;
+                    LocalToWorld muzzleLocalToWorld = GetComponent<LocalToWorld>(railgun._muzzleEntity);
 
-                    if (weapon.ShootRequested && railgun._firingTimer <= 0f)
+                    // Select shoot raycast origin point
+                    LocalToWorld raycastOriginEntityLocalToWorld = muzzleLocalToWorld;
+                    if (HasComponent<LocalToWorld>(weapon.ShootOriginOverride))
                     {
-                        LocalToWorld muzzleLocalToWorld = GetComponent<LocalToWorld>(railgun._muzzleEntity);
-
-                        // Select shoot raycast origin point
-                        LocalToWorld raycastOriginEntityLocalToWorld = muzzleLocalToWorld;
-                        if (HasComponent<LocalToWorld>(weapon.ShootOriginOverride))
-                        {
-                            raycastOriginEntityLocalToWorld = GetComponent<LocalToWorld>(weapon.ShootOriginOverride);
-                        }
-
-                        float3 shotLazerPosition = muzzleLocalToWorld.Position;
-                        float3 shotHitPosition = shotLazerPosition + (muzzleLocalToWorld.Forward * railgun.Range);
-                        bool shotHasHit = false;
-
-                        tmpRaycastHits.Clear();
-                        RaycastInput rayInput = new RaycastInput
-                        {
-                            Filter = CollisionFilter.Default,
-                            Start = raycastOriginEntityLocalToWorld.Position,
-                            End = raycastOriginEntityLocalToWorld.Position + (raycastOriginEntityLocalToWorld.Forward * railgun.Range),
-                        };
-                        if (collisionWorld.CastRay(rayInput, ref tmpRaycastHits))
-                        {
-                            // Select hit
-                            RaycastHit closestValidHit = default;
-                            closestValidHit.Fraction = float.MaxValue;
-                            for (int i = 0; i < tmpRaycastHits.Length; i++)
-                            {
-                                RaycastHit tmpHit = tmpRaycastHits[i];
-
-                                if (tmpHit.Entity != weapon.OwnerEntity)
-                                {
-                                    if (tmpHit.Fraction < closestValidHit.Fraction)
-                                    {
-                                        closestValidHit = tmpHit;
-                                    }
-                                }
-                            }
-
-                            if (closestValidHit.Entity != Entity.Null)
-                            {
-                                // Apply damage
-                                if (isServer)
-                                {
-                                    if (HasComponent<Health>(closestValidHit.Entity))
-                                    {
-                                        Health health = GetComponent<Health>(closestValidHit.Entity);
-                                        health.CurrentHealth -= railgun.Damage;
-                                        SetComponent(closestValidHit.Entity, health);
-                                    }
-                                }
-
-                                shotHasHit = true;
-                                shotHitPosition = closestValidHit.Position;
-                            }
-                        }
-
-                        quaternion shotLazerRotation = quaternion.LookRotationSafe(math.normalizesafe(shotHitPosition - muzzleLocalToWorld.Position), muzzleLocalToWorld.Up);
-
-                        // Shot visuals
-                        // Only ask for shot visuals if we didn't already spawn shot visuals at that tick
-                        if (!isServer && tick > railgun._lastTickShot)
-                        {
-                            // Spawn hit vfx
-                            if (shotHasHit && railgun.HitSparkPrefab != Entity.Null)
-                            {
-                                for (int s = 0; s < railgun.HitSparksCount; s++)
-                                {
-                                    Entity hitVfxEntity = commandBuffer.Instantiate(railgun.HitSparkPrefab);
-                                    commandBuffer.SetComponent(hitVfxEntity, new Translation { Value = shotHitPosition });
-                                    commandBuffer.SetComponent(hitVfxEntity, new Rotation { Value = sparksRandom.NextQuaternionRotation() });
-                                }
-                            }
-
-                            // Spawn shoot vfx
-                            if (railgun.LazerPrefab != Entity.Null)
-                            {
-                                Entity shootVfxEntity = commandBuffer.Instantiate(railgun.LazerPrefab);
-                                commandBuffer.SetComponent(shootVfxEntity, new Translation { Value = shotLazerPosition });
-                                commandBuffer.SetComponent(shootVfxEntity, new Rotation { Value = shotLazerRotation });
-
-                                NonUniformScale shootVfxScale = GetComponent<NonUniformScale>(railgun.LazerPrefab);
-                                shootVfxScale.Value.z = math.distance(shotLazerPosition, shotHitPosition);
-                                commandBuffer.SetComponent(shootVfxEntity, shootVfxScale);
-                            }
-
-                            // Handle recoil
-                            if (HasComponent<FirstPersonCharacterComponent>(weapon.OwnerEntity))
-                            {
-                                FirstPersonCharacterComponent owningCharacter = GetComponent<FirstPersonCharacterComponent>(weapon.OwnerEntity);
-
-                                owningCharacter.RecoilVector += -math.forward() * railgun.Recoil;
-                                owningCharacter.RecoilVector = MathUtilities.ClampToMaxLength(owningCharacter.RecoilVector, owningCharacter.RecoilMaxDistance);
-
-                                owningCharacter.TargetRecoilFOVKick += railgun.RecoilFOVKick;
-                                owningCharacter.TargetRecoilFOVKick = math.clamp(owningCharacter.TargetRecoilFOVKick, 0f, owningCharacter.RecoilMaxFOVKick);
-
-                                SetComponent(weapon.OwnerEntity, owningCharacter);
-                            }
-                        }
-                        railgun._lastTickShot = tick;
-
-                        railgun._firingTimer = 1f / railgun.FireRate;
+                        raycastOriginEntityLocalToWorld = GetComponent<LocalToWorld>(weapon.ShootOriginOverride);
                     }
+
+                    float3 shotLazerPosition = muzzleLocalToWorld.Position;
+                    float3 shotHitPosition = shotLazerPosition + (muzzleLocalToWorld.Forward * railgun.Range);
+                    bool shotHasHit = false;
+
+                    tmpRaycastHits.Clear();
+                    RaycastInput rayInput = new RaycastInput
+                    {
+                        Filter = CollisionFilter.Default,
+                        Start = raycastOriginEntityLocalToWorld.Position,
+                        End = raycastOriginEntityLocalToWorld.Position + (raycastOriginEntityLocalToWorld.Forward * railgun.Range),
+                    };
+                    if (collisionWorld.CastRay(rayInput, ref tmpRaycastHits))
+                    {
+                        // Select hit
+                        RaycastHit closestValidHit = default;
+                        closestValidHit.Fraction = float.MaxValue;
+                        for (int i = 0; i < tmpRaycastHits.Length; i++)
+                        {
+                            RaycastHit tmpHit = tmpRaycastHits[i];
+
+                            if (tmpHit.Entity != weapon.OwnerEntity)
+                            {
+                                if (tmpHit.Fraction < closestValidHit.Fraction)
+                                {
+                                    closestValidHit = tmpHit;
+                                }
+                            }
+                        }
+
+                        if (closestValidHit.Entity != Entity.Null)
+                        {
+
+                            if (HasComponent<Health>(closestValidHit.Entity))
+                            {
+                                Health health = GetComponent<Health>(closestValidHit.Entity);
+                                health.CurrentHealth -= railgun.Damage;
+                                SetComponent(closestValidHit.Entity, health);
+                            }
+
+
+                            shotHasHit = true;
+                            shotHitPosition = closestValidHit.Position;
+                        }
+                    }
+
+                    quaternion shotLazerRotation = quaternion.LookRotationSafe(math.normalizesafe(shotHitPosition - muzzleLocalToWorld.Position), muzzleLocalToWorld.Up);
+
+                    // Shot visuals
+
+                    // Spawn hit vfx
+                    if (shotHasHit && railgun.HitSparkPrefab != Entity.Null)
+                    {
+                        for (int s = 0; s < railgun.HitSparksCount; s++)
+                        {
+                            Entity hitVfxEntity = commandBuffer.Instantiate(railgun.HitSparkPrefab);
+                            commandBuffer.SetComponent(hitVfxEntity, new Translation { Value = shotHitPosition });
+                            commandBuffer.SetComponent(hitVfxEntity, new Rotation { Value = sparksRandom.NextQuaternionRotation() });
+                        }
+                    }
+
+                    // Spawn shoot vfx
+                    if (railgun.LazerPrefab != Entity.Null)
+                    {
+                        Entity shootVfxEntity = commandBuffer.Instantiate(railgun.LazerPrefab);
+                        commandBuffer.SetComponent(shootVfxEntity, new Translation { Value = shotLazerPosition });
+                        commandBuffer.SetComponent(shootVfxEntity, new Rotation { Value = shotLazerRotation });
+
+                        NonUniformScale shootVfxScale = GetComponent<NonUniformScale>(railgun.LazerPrefab);
+                        shootVfxScale.Value.z = math.distance(shotLazerPosition, shotHitPosition);
+                        commandBuffer.SetComponent(shootVfxEntity, shootVfxScale);
+                    }
+
+                    // Handle recoil
+                    if (HasComponent<FirstPersonCharacterComponent>(weapon.OwnerEntity))
+                    {
+                        FirstPersonCharacterComponent owningCharacter = GetComponent<FirstPersonCharacterComponent>(weapon.OwnerEntity);
+
+                        owningCharacter.RecoilVector += -math.forward() * railgun.Recoil;
+                        owningCharacter.RecoilVector = MathUtilities.ClampToMaxLength(owningCharacter.RecoilVector, owningCharacter.RecoilMaxDistance);
+
+                        owningCharacter.TargetRecoilFOVKick += railgun.RecoilFOVKick;
+                        owningCharacter.TargetRecoilFOVKick = math.clamp(owningCharacter.TargetRecoilFOVKick, 0f, owningCharacter.RecoilMaxFOVKick);
+
+                        SetComponent(weapon.OwnerEntity, owningCharacter);
+                    }
+
+                    railgun._firingTimer = 1f / railgun.FireRate;
                 }
+
             }).Schedule(Dependency);
 
         WeaponCommandBufferSystem.AddJobHandleForProducer(Dependency);
